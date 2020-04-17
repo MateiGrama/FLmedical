907f5ac903771f53d2b989a259ae8f32a48b8e63
Matei Grama <mgg17@ic.ac.uk>
2020-04-17 10:50:32 +0100

    Rectified paths to match gpucluster vm.

--- runExperimentGPU.sh
+++ runExperimentGPU.sh
@@ -3,7 +3,7 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=mgg17
 
-source /home/mgg17/diss/venv/bin/activate
+source /vol/bitbucket/mgg17/diss/venv/bin/activate
 source /vol/cuda/10.0.130/setup.sh
 TERM=vt100
 /usr/bin/nvidia-smi
