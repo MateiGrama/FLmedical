e7569909f3f361864e5161e7fa0664b9f3641765
Matei Grama <matei.grama.george@gmail.com>
2020-05-25 16:01:03 +0300

    Renamed output file.

--- runExperimentGPU.sh
+++ runExperimentGPU.sh
@@ -13,7 +13,8 @@ TERM=vt100
 
 uptime
 
-nohup python -u main.py > out/experimentCOVIDx_resnet_5clients_2batch_10rounds_cpu.log 2>&1
+nohup python -u main.py > out/experimentDiabetes.log 2>&1
+#nohup python -u main.py > out/experimentCOVIDx_resnet_5clients_2batch_10rounds_cpu.log 2>&1
 
 #echo $! > out/lastExperimentPID.txt
 
